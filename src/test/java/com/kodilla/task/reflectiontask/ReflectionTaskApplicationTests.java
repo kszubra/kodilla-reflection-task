982341153066e412d704410251ca4b5ccaab8639
Kacper Szubra <kacper.szubra@nvt.pl>
2021-10-16 16:36:15 +0200

    initial commit

---
+++ src/test/java/com/kodilla/task/reflectiontask/ReflectionTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.task.reflectiontask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReflectionTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
